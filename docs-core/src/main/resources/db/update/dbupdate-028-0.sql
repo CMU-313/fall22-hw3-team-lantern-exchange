476fb65008e8d5b79804c6bcce52a1e585890586
JitongZ <justin_zhoujitong@163.com>
2022-10-05 15:22:05 -0400

    updated database column initialization for new fields

--- docs-core/src/main/resources/db/update/dbupdate-028-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-028-0.sql
@@ -1,5 +1,5 @@
-alter table T_DOCUMENT add column DOC_GPA_C varchar(400);
-alter table T_DOCUMENT add column DOC_SKILLS_C varchar(400);
-alter table T_DOCUMENT add column DOC_EXPERIENCE_C varchar(400);
-alter table T_DOCUMENT add column DOC_EDUCATION_C varchar(400);
+alter table T_DOCUMENT add column DOC_GPA_C varchar(500) not null default '';
+alter table T_DOCUMENT add column DOC_SKILLS_C varchar(500) not null default '';
+alter table T_DOCUMENT add column DOC_EXPERIENCE_C varchar(500) not null default '';
+alter table T_DOCUMENT add column DOC_EDUCATION_C varchar(500) not null default '';
 update T_CONFIG set CFG_VALUE_C = '28' where CFG_ID_C = 'DB_VERSION';
