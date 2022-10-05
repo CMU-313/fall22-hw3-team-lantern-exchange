2b6695152b8449130811bad60a0989fedef87d51
Peggy Shen <onlyanothernobody@gmail.com>
2022-10-04 23:43:24 -0400

    Added document js file for dashboard

---
+++ docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewDashboard.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Document view dashboard controller.
+ * Added as part of feature review dashboard.
+ */
+
+ angular.module('docs').controller('DocumentViewDashboard', function($scope) {
+    // Add code into controller
+
+    // Submitting scores
+
+  });
