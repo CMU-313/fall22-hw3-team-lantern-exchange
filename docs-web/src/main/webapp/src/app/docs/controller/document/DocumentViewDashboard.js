fac0c0cac3667775ceb93559dc671c80ad17edf5
Peggy Shen <onlyanothernobody@gmail.com>
2022-10-06 14:58:16 -0400

    Updated aggregate scoring with average instead of addition

--- docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewDashboard.js
+++ docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewDashboard.js
@@ -12,11 +12,25 @@
     angular.module('dashboardApp', [])
       .controller('DashboardCtrl', ['$scope',
         function($scope) {
-          $scope.GPAOptions = [1,2,3,4,5,6,7,8,9,10];
-          $scope.result = 0;
+          $scope.GPAOptions = [
+            {score:1},
+            {score:2},
+            {score:3},
+            {score:4},
+            {score:5},
+            {score:6},
+            {score:7},
+            {score:8},
+            {score:9},
+            {score:10}];
+          $scope.submittedScore = 0;
+          $scope.reviewCount = 0;
+          $scope.GPAavg = 0;
           //$scope.myModel = 1; // note it's an Integer
           $scope.checkselection = function () {
-            $scope.result = $scope.GPASelect + 2
+            $scope.submittedScore = $scope.GPASelect.score + $scope.submittedScore;
+            $scope.reviewCount = $scope.reviewCount + 1;
+            $scope.GPAavg = $scope.submittedScore / $scope.reviewCount;
           }
  
         }
