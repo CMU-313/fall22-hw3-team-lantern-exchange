d71af43870626cbfedf98913de89e33bed1b8fff
Peggy Shen <onlyanothernobody@gmail.com>
2022-10-06 03:36:12 -0400

    Added angularjs for GPA selection

--- docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewDashboard.js
+++ docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewDashboard.js
@@ -1,13 +1,28 @@
-'use strict';
-
 /**
  * Document view dashboard controller.
  * Added as part of feature review dashboard.
  */
 
- angular.module('docs').controller('DocumentViewDashboard', function($scope) {
-    // Add code into controller
+ //angular.module('docs').controller('DocumentViewDashboard', function($scope) {
+    // Convert string from dropdown to number
+    //var numval = document.getElementById("gpa");
+    //var value = numval.value;
 
     // Submitting scores
+    angular.module('dashboardApp', [])
+      .controller('DashboardCtrl', ['$scope',
+        function($scope) {
+          $scope.GPAOptions = [1,2,3,4,5,6,7,8,9,10];
+          $scope.result = 0;
+          //$scope.myModel = 1; // note it's an Integer
+          $scope.checkselection = function () {
+            $scope.result = $scope.GPASelect + 2
+          }
+ 
+        }
+      ]);
 
-  });
+
+    // Average of scores
+
+  //});
