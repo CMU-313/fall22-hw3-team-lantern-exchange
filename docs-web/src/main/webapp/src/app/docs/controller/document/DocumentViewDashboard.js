36d08a98271e27c7dfd07537af1ffe2d90c5f7e0
peggy <98036446+shenpeg@users.noreply.github.com>
2022-10-06 23:24:42 -0400

    Delete redundant js dashboard code

--- docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewDashboard.js
+++ docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewDashboard.js
@@ -60,32 +60,4 @@
     }
 
 
-    angular.module('dashboardApp', [])
-    .controller('DashboardCtrl', ['$scope',
-      function($scope) {
-        $scope.GPAOptions = [
-          {score:1},
-          {score:2},
-          {score:3},
-          {score:4},
-          {score:5},
-          {score:6},
-          {score:7},
-          {score:8},
-          {score:9},
-          {score:10}];
-        $scope.submittedScore = 0;
-        $scope.reviewCount = 0;
-        $scope.GPAavg = 0;
-        //$scope.myModel = 1; // note it's an Integer
-        $scope.checkselection = function () {
-          $scope.submittedScore = $scope.GPASelect.score + $scope.submittedScore;
-          $scope.reviewCount = $scope.reviewCount + 1;
-          $scope.GPAavg = $scope.submittedScore / $scope.reviewCount;
-        }
-
-      }
-
-      
-    ]);
 
